d3088a84d06394de4de4a9f9f7e4186bd43fb58c
Siddharth Mahajan <playmast3r@users.noreply.github.com>
2016-10-10 16:04:03 +0530

    Added comments

--- sqlsafe2.php
+++ sqlsafe2.php
@@ -1,16 +1,20 @@
 <?php
-
+//this is another workaround for safeguarding against sql injection
+//parameterized query is 100% secure. But if you don't want to use it, then use this inbuilt mysqli_real_escape_string() function
+//but this is not as secure as parameterized query
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'null');
+   //connect to database
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   //always set the charset of the mysql connection, as attacker can use utf-7 and easily bypass the escaping.
    mysqli_set_charset($db, "utf8");
    
    $input = "'sql injection";
    
-   echo $input;
+   echo $input;  //will print 'sql injection
    echo "<br>";
-   echo mysqli_real_escape_string($db, $input);
+   echo mysqli_real_escape_string($db, $input);   //will print \'sql injection
    
    ?>
