8df9a724c5ff03383a26faa61c4ca8fad02f38ec
Siddharth Mahajan <playmast3r@users.noreply.github.com>
2016-10-10 15:41:10 +0530

    Added comments

--- image-attack.php
+++ image-attack.php
@@ -1,5 +1,7 @@
 <?php
-
+//Use exif editor software to add php script in "DocumentName" tag like: <?php echo "vulnerability check"; phpinfo(); __halt_compiler();
+//and after that if you try to run this code and include that image file, the php code will get invoked and your above script which you added in
+//the exif tag will get executed
 require("image-safe.jpg");
 
 ?>
